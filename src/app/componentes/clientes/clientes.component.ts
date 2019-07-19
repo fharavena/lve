a0ee7d613436e9d06be50f9b62ac5dcac7ff9079
fharavena <fharavena@gmail.com>
2019-07-19 02:33:07 -0400

    trabajo en componente cliente

---
+++ src/app/componentes/clientes/clientes.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ClienteServicio } from 'src/app/servicios/cliente.service';
+import { Cliente } from 'src/app/modelo/cliente.model';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
+})
+export class ClientesComponent implements OnInit {
+
+clientes: Cliente[];
+cliente: Cliente = {
+  nombre: '',
+  apellido: '',
+  email: '',
+  saldo: 0
+};
+
+  constructor(private clientesServicio: ClienteServicio,
+    private flashMessages: FlashMessagesService) { }
+
+  ngOnInit() {
+    this.clientesServicio.getClientes().subscribe(
+      clientes=>{
+        this.clientes = clientes;
+      }
+    );
+  }
+
+  getSaldoTotal(){
+    let saldoTotal: number =0;
+    if (this.clientes) {
+      this.clientes.forEach(clientes =>{
+        saldoTotal += clientes.saldo;
+      });
+    }
+    return saldoTotal;
+  }
+
+  agregar({value, valid}: {value: Cliente, valid: boolean}) {
+    if (!valid) {
+      this.flashMessages.show('Por favor llena el formulario correctamente', {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    } else {
+      // Agregamos nuevo cliente
+    }
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import { Llaves } from './config';
+
 export const environment = {
-  production: true
+  production: true,
+  firestore:{
+    apiKey: Llaves.apiKey,
+    authDomain: Llaves.authDomain,
+    databaseURL: Llaves.databaseURL,
+    projectId: Llaves.projectId,
+    storageBucket: Llaves.storageBucket,
+    messagingSenderId: Llaves.messagingSenderId,
+    appId: Llaves.appId
+  }
 };
